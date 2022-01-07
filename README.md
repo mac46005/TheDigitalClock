cadb561e2382fa6420a45cc788fcec86f4a4a9a9
Marco Preciado <mac46005@gmail.com>
2022-01-07 12:14:06 -0600

    Learned about the ViewBox funtion

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+![TheDigitalClock](The_Digital_Clock.png)
+
 # TheDigitalClock
 Very Simple Digital Clock
+
+
+## About  
+A very simple wpf project of a digital clock :)
---
+++ TheDigitalClock_WPF_UI/MVVM/ViewModels/ShellViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDigitalClock_WPF_UI.MVVM.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        IEventAggregator _events;
+        public ShellViewModel(IEventAggregator events)
+        {
+            _events = events;
+            _events.Subscribe(this);
+        }
+    }
+}
