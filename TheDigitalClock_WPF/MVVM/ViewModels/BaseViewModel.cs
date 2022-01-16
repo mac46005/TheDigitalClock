89eaac2bc146b954a952b189603fab1307a21453
Marco Preciado <mac46005@gmail.com>
2022-01-16 08:32:17 -0600

    BaseViewModel

---
+++ TheDigitalClock_WPF/MVVM/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDigitalClock_WPF.MVVM.ViewModels
+{
+    public class BaseViewModel : IViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Dispose()
+        {
+            // dispose stuff in here yo
+        }
+    }
+}
--- TheDigitalClock_WPF/MVVM/ViewModels/IViewModel.cs
+++ TheDigitalClock_WPF/MVVM/ViewModels/IViewModel.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TheDigitalClock_WPF.MVVM.Views
+namespace TheDigitalClock_WPF.MVVM.ViewModels
 {
-    public interface IViewModel : INotifyPropertyChanged,IDisposable
+    public interface IViewModel : INotifyPropertyChanged, IDisposable
     {
     }
 }
