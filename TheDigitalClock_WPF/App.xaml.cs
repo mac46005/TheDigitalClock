e55e52677b2fa90446a784ac69edb4ec8148e84c
Marco Preciado <mac46005@gmail.com>
2022-01-16 08:14:27 -0600

    Restart without using caliburn micro

--- TheDigitalClock_WPF/App.xaml.cs
+++ TheDigitalClock_WPF/App.xaml.cs
@@ -13,23 +13,5 @@ namespace TheDigitalClock_WPF
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            Bootstrapper.Start();
-
-            Window window = new MainWindow();
-
-            window.DataContext = Bootstrapper.RootVisual;
-
-            window.Closed += Window_Closed;
-
-            window.Show();
-        }
-
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            Bootstrapper.Stop();
-        }
     }
 }
--- TheDigitalClock_WPF/MVVM/ViewModels/IMainViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using TheDigitalClock_WPF.Core.MVVMHelper;
-
-namespace TheDigitalClock_WPF.MVVM.ViewModels
-{
-    public interface IMainViewModel : IViewModel
-    {
-        object CurrentView { get; set; }
-    }
-}
