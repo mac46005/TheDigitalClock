b05b7143e47ac5a33eed215c985a962a473aff2a
Marco Preciado <mac46005@gmail.com>
2022-01-16 08:28:31 -0600

    IViewModel

---
+++ TheDigitalClock_WPF/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDigitalClock_WPF
+{
+    public static class Bootstrapper
+    {
+        private static ILifetimeScope _rootScope;
+
+        public static void StartConfiguration()
+        {
+            if(_rootScope != null)
+            {
+                return;
+            }
+
+            var builder = new ContainerBuilder();
+
+            var assemblies = new[] { Assembly.GetExecutingAssembly() };
+
+            builder.RegisterAssemblyTypes(assemblies);
+        }
+        
+
+
+    }
+}
